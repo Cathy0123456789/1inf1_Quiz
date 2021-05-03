ba670eab1d5bff2257d25ae48f2c0b222f800f41
Cathy0123456789 <redgreenwhite.fish@gmail.com>
2021-05-03 14:44:35 +0200

    Klasse KATEGORIE umgesetzt

--- KATEGORIE.java
+++ KATEGORIE.java
@@ -4,14 +4,24 @@ class KATEGORIE
 {
     String name;
     ArrayList<FRAGE> fragen;
-    
-    KATEGORIE()
-    {
-        
-    }
-    
-    // FRAGE Zufallsfrage()
-    // {
 
-    // }
+    KATEGORIE(String kategorieName)
+    {
+        name = kategorieName;
+        ArrayList<FRAGE> fragen = new ArrayList<FRAGE>();
+    }
+
+    void Zufallszahl()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(5 + (int)(Math.random() * (5 + 1)));
+        }
+    }
+
+    FRAGE Zufallsfrage()
+    {
+        int x = fragen.size() + (int)(Math.random() * (fragen.size() + 1));
+        return fragen.get(x);
+    }
 }
