81c126d78b4b1ee88543f5c362f8736affd22a9b
Cathy0123456789 <redgreenwhite.fish@gmail.com>
2021-05-04 00:10:04 +0200

    Methode Zufallsfrage() in KATEGORIE aktualisiert

--- KATEGORIE.java
+++ KATEGORIE.java
@@ -11,17 +11,9 @@ class KATEGORIE
         ArrayList<FRAGE> fragen = new ArrayList<FRAGE>();
     }
 
-    void Zufallszahl()
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            System.out.println(5 + (int)(Math.random() * (5 + 1)));
-        }
-    }
-
     FRAGE Zufallsfrage()
     {
-        int x = fragen.size() + (int)(Math.random() * (fragen.size() + 1));
+        int x = (int)(Math.random() * (fragen.size() + 1));
         return fragen.get(x);
     }
 }
