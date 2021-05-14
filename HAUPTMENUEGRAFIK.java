f98ba7a5ee0d60d10c750d46bfd1a954f966b870
Cathy0123456789 <redgreenwhite.fish@gmail.com>
2021-05-14 15:23:17 +0200

    Start Button in HAUPTMENUEGRAFIK hinzugefuegt

--- HAUPTMENUEGRAFIK.java
+++ HAUPTMENUEGRAFIK.java
@@ -5,7 +5,8 @@ class HAUPTMENUEGRAFIK
 {
     Frame hauptfenster;
     Label begrueßung;
-   
+    Button start;
+
     HAUPTMENUEGRAFIK ()
     {
         hauptfenster = new Frame();
@@ -13,16 +14,23 @@ class HAUPTMENUEGRAFIK
         hauptfenster.setLocation(10,50);
         hauptfenster.setVisible(true);
         hauptfenster.setLayout(null);
-        
-        
+
         begrueßung = new Label();
         begrueßung.setSize(200,50);
         begrueßung.setLocation(325,150);
         begrueßung.setVisible(true);
         begrueßung.setText("Herzlich Willkommen und viel Spaß!");
         begrueßung.setEnabled(true);
-        
+
         hauptfenster.add(begrueßung);
-    
+
+        start = new Button();
+        start.setSize(120,50);
+        start.setLocation(375,250);
+        start.setVisible(true);
+        start.setLabel("Spiel starten");
+        start.setEnabled(true);
+
+        hauptfenster.add(start);
     }
 }
