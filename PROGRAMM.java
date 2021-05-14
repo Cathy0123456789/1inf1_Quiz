25f3aba03417f6b0a7793bfd3141a7b7d094b122
Cathy0123456789 <redgreenwhite.fish@gmail.com>
2021-05-14 14:54:37 +0200

    Grafikoberflaeche in PROGRAMM eingebunden

--- PROGRAMM.java
+++ PROGRAMM.java
@@ -9,14 +9,20 @@ class PROGRAMM
     {
         // neues Quiz mit 5 Fragen
         quiz = new QUIZ(5);
-        
+
         // Kategorien aus dem Ordner "Fragen" einlesen
         quiz.KategorienEinlesen();
 
-        // in jeder Kategorie alle Fragen im jeweiligen Ordner einleen
+        // in jeder Kategorie alle Fragen im jeweiligen Ordner einlesen
         for (KATEGORIE kat : quiz.kategorien)
         {
             kat.FragenEinlesen();
         }
+
+        
+        KATEGORIE k = quiz.kategorien.get(0);
+        FRAGE f = k.Zufallsfrage();
+        
+        grafik = new GRAFIKOBERFLAECHE();
     }
 }
