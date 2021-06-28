33fe51873c3e155a9e409b7a15d08ad3663d3d81
Cathy0123456789 <redgreenwhite.fish@gmail.com>
2021-06-28 14:44:43 +0200

    Frage verbessert Button in ENDGRAFIK hinzugefuegt

--- ENDGRAFIK.java
+++ ENDGRAFIK.java
@@ -3,12 +3,13 @@ import java.awt.event.*;
 
 class ENDGRAFIK
 {
-    Frame hauptfenster;
+    Frame frame;
     Label ende;
+    Button start;
 
-    ENDGRAFIK (Frame hauptf, int breite, int hoehe, int punkte)
+    ENDGRAFIK (Frame frame, int breite, int hoehe, int punkte)
     {
-        hauptfenster = hauptf;
+        this.frame = frame;
 
         ende = new Label();
         ende.setSize(breite, hoehe/20);
@@ -18,7 +19,34 @@ class ENDGRAFIK
         ende.setText(String.format("Ende :) Du hast %d Punkte erreicht.", punkte));
         ende.setVisible(true);
         ende.setEnabled(true);
+        
+        frame.add(ende);
+        
+        // Button um zurueck zum Hauptmenue zu gelangen
+        start = new Button();
+        start.setSize(breite/10, hoehe/15);
+        start.setLocation((int) (0.45 * breite), (int) (0.45 * hoehe));
+        start.setLabel("Neues Spiel starten");
+        start.setFont(new Font("Neu", Font.PLAIN, start.getSize().height/5));
+        start.setVisible(true);
+        start.setEnabled(true);
+        
+        start.addMouseListener(new MouseListener()
+            {
+                public void mousePressed(MouseEvent me) {}
 
-        hauptfenster.add(ende);
+                public void mouseReleased(MouseEvent me) {}
+
+                public void mouseClicked(MouseEvent me)
+                {
+                    frame.removeAll();
+                }
+
+                public void mouseExited(MouseEvent me) {}
+
+                public void mouseEntered(MouseEvent me) {}
+            });
+            
+        frame.add(start);
     }
 }
