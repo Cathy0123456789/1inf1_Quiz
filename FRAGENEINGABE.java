863b9d6175e8ce1db4a53144ce1f047f6b007f01
Cathy0123456789 <redgreenwhite.fish@gmail.com>
2021-07-08 19:42:26 +0200

    Frageneingabe über BlueJ funktioniert

--- FRAGENEINGABE.java
+++ FRAGENEINGABE.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.File;
 
 class FRAGENEINGABE
 {
@@ -19,20 +21,27 @@ class FRAGENEINGABE
             System.out.println("Antwort " + i + ":");
             antworten[i-1] = scanner.nextLine();
         }
-        
+
         System.out.println("Nummer der richtigen Antwort:");
         int richtigeAntwort = Integer.parseInt(scanner.nextLine());
-        
+
         System.out.println("Schweregrad der Frage:");
         int schweregrad = Integer.parseInt(scanner.nextLine());
-        
+
         System.out.println("Frage abspeichern? Ja / Nein");
         if (scanner.nextLine().equals("Ja"))
         {
-            DateiSchreiben(frage, antworten, richtigeAntwort, schweregrad);
+            System.out.println("In welcher Kategorie?");
+            for (int i = 1; i <= KategorienEinlesen().size(); i++)
+            {
+                System.out.println(i + " : " + KategorienEinlesen().get(i-1));
+            }
+            System.out.println("Bitte Nummer der Kategorie eingeben.");
+            String kategorie = KategorienEinlesen().get(Integer.parseInt(scanner.nextLine())-1);
+            DateiErstellen(kategorie, frage, antworten, richtigeAntwort, schweregrad);
         }
-        
-        System.out.println("Neue Frage hinzufügen? Ja / Nein");
+
+        System.out.println("\nNeue Frage hinzufügen? Ja / Nein");
         String neueFrage = scanner.nextLine();
         if (neueFrage.equals("Ja"))
         {
@@ -45,20 +54,52 @@ class FRAGENEINGABE
         }
     }
 
-    private static void DateiSchreiben(String frage, String[] antworten, int richtigeAntwort, int schweregrad)
+    private static void DateiErstellen(String kategorie, String frage, String[] antworten, int richtigeAntwort, int schweregrad)
     {
-        // try
-        // {
-            // PrintWriter writer = new PrintWriter(new FileWriter(datei));
+        File kat = new File("Fragen\\" + kategorie);
+        File f = new File(kat + "\\frage" + (kat.list().length + 1) + ".txt");
 
-            // writer.println();
+        try
+        {
+            f.createNewFile();
 
-            // writer.close();
-        // }
-        // catch (IOException e)
-        // {
-            // System.out.println("Fehler aufgetreten.");
-            // e.printStackTrace();
-        // }
+            PrintWriter writer = new PrintWriter(new FileWriter(f));
+
+            writer.println(frage);
+            for (String i : antworten)
+            {
+                writer.println(i);
+            }
+            writer.println(richtigeAntwort);
+            writer.print(schweregrad);
+            
+            writer.close();
+            
+            System.out.println("Die Frage wurde erfolgreich abgespeichert.");
+        }
+        catch (IOException e)
+        {
+            System.out.println("Fehler aufgetreten.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Liest Ordner-Namen (= Kategorien) aus dem Ordner "Fragen" ein
+     */
+
+    private static ArrayList<String> KategorienEinlesen()
+    {
+        File[] directories = new File("Fragen").listFiles(File::isDirectory);
+
+        ArrayList<String> kategorien = new ArrayList<String>();
+
+        for (File i : directories)
+        {
+            String kat = i.toString();
+            kategorien.add(kat.substring(7, kat.length()));
+        }
+
+        return kategorien;
     }
 }
